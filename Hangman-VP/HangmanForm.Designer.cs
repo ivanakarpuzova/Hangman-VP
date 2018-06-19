21922320843d298ee8938f820ed1238bb0b6e12b
natasastojanova <nateserenay@gmail.com>
2018-06-19 14:22:57 +0200

    Nova forma za besilkata i sliki za coveceto

---
+++ Hangman-VP/HangmanForm.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Hangman_VP
+{
+    partial class HangmanForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.hangmanLeftLeg = new System.Windows.Forms.PictureBox();
+            this.hangmanBody = new System.Windows.Forms.PictureBox();
+            this.hangmanHead = new System.Windows.Forms.PictureBox();
+            this.hangmanRightArm = new System.Windows.Forms.PictureBox();
+            this.hangmanLeftArm = new System.Windows.Forms.PictureBox();
+            this.hangmanRightLeg = new System.Windows.Forms.PictureBox();
+            this.BasePictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanLeftLeg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanBody)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanHead)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanRightArm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanLeftArm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanRightLeg)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasePictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // hangmanLeftLeg
+            // 
+            this.hangmanLeftLeg.Location = new System.Drawing.Point(440, 377);
+            this.hangmanLeftLeg.Margin = new System.Windows.Forms.Padding(2);
+            this.hangmanLeftLeg.Name = "hangmanLeftLeg";
+            this.hangmanLeftLeg.Size = new System.Drawing.Size(83, 125);
+            this.hangmanLeftLeg.TabIndex = 27;
+            this.hangmanLeftLeg.TabStop = false;
+            this.hangmanLeftLeg.Visible = false;
+            // 
+            // hangmanBody
+            // 
+            this.hangmanBody.Location = new System.Drawing.Point(506, 204);
+            this.hangmanBody.Margin = new System.Windows.Forms.Padding(2);
+            this.hangmanBody.Name = "hangmanBody";
+            this.hangmanBody.Size = new System.Drawing.Size(17, 176);
+            this.hangmanBody.TabIndex = 24;
+            this.hangmanBody.TabStop = false;
+            this.hangmanBody.Visible = false;
+            // 
+            // hangmanHead
+            // 
+            this.hangmanHead.Location = new System.Drawing.Point(468, 113);
+            this.hangmanHead.Margin = new System.Windows.Forms.Padding(2);
+            this.hangmanHead.Name = "hangmanHead";
+            this.hangmanHead.Size = new System.Drawing.Size(110, 94);
+            this.hangmanHead.TabIndex = 23;
+            this.hangmanHead.TabStop = false;
+            this.hangmanHead.Visible = false;
+            // 
+            // hangmanRightArm
+            // 
+            this.hangmanRightArm.Location = new System.Drawing.Point(505, 212);
+            this.hangmanRightArm.Margin = new System.Windows.Forms.Padding(2);
+            this.hangmanRightArm.Name = "hangmanRightArm";
+            this.hangmanRightArm.Size = new System.Drawing.Size(93, 136);
+            this.hangmanRightArm.TabIndex = 26;
+            this.hangmanRightArm.TabStop = false;
+            this.hangmanRightArm.Visible = false;
+            // 
+            // hangmanLeftArm
+            // 
+            this.hangmanLeftArm.Location = new System.Drawing.Point(432, 218);
+            this.hangmanLeftArm.Margin = new System.Windows.Forms.Padding(2);
+            this.hangmanLeftArm.Name = "hangmanLeftArm";
+            this.hangmanLeftArm.Size = new System.Drawing.Size(76, 130);
+            this.hangmanLeftArm.TabIndex = 25;
+            this.hangmanLeftArm.TabStop = false;
+            this.hangmanLeftArm.Visible = false;
+            // 
+            // hangmanRightLeg
+            // 
+            this.hangmanRightLeg.Location = new System.Drawing.Point(499, 377);
+            this.hangmanRightLeg.Margin = new System.Windows.Forms.Padding(2);
+            this.hangmanRightLeg.Name = "hangmanRightLeg";
+            this.hangmanRightLeg.Size = new System.Drawing.Size(99, 136);
+            this.hangmanRightLeg.TabIndex = 28;
+            this.hangmanRightLeg.TabStop = false;
+            this.hangmanRightLeg.Visible = false;
+            // 
+            // BasePictureBox
+            // 
+            this.BasePictureBox.Location = new System.Drawing.Point(499, 36);
+            this.BasePictureBox.Margin = new System.Windows.Forms.Padding(2);
+            this.BasePictureBox.Name = "BasePictureBox";
+            this.BasePictureBox.Size = new System.Drawing.Size(313, 466);
+            this.BasePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BasePictureBox.TabIndex = 29;
+            this.BasePictureBox.TabStop = false;
+            // 
+            // HangmanForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(832, 528);
+            this.Controls.Add(this.hangmanLeftLeg);
+            this.Controls.Add(this.hangmanBody);
+            this.Controls.Add(this.hangmanHead);
+            this.Controls.Add(this.hangmanRightArm);
+            this.Controls.Add(this.hangmanLeftArm);
+            this.Controls.Add(this.hangmanRightLeg);
+            this.Controls.Add(this.BasePictureBox);
+            this.Name = "HangmanForm";
+            this.Text = "HangmanForm";
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanLeftLeg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanBody)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanHead)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanRightArm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanLeftArm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.hangmanRightLeg)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BasePictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox hangmanLeftLeg;
+        private System.Windows.Forms.PictureBox hangmanBody;
+        private System.Windows.Forms.PictureBox hangmanHead;
+        private System.Windows.Forms.PictureBox hangmanRightArm;
+        private System.Windows.Forms.PictureBox hangmanLeftArm;
+        private System.Windows.Forms.PictureBox hangmanRightLeg;
+        private System.Windows.Forms.PictureBox BasePictureBox;
+    }
+}
---
+++ Hangman-VP/HangmanForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hangman_VP
+{
+    public partial class HangmanForm : Form
+    {
+        public HangmanForm()
+        {
+            InitializeComponent();
+
+            BasePictureBox.ImageLocation = @"Images\base.png";
+            hangmanHead.ImageLocation = @"Images\hangman-1.png";
+            hangmanBody.ImageLocation = @"Images\hangman-2.png";
+            hangmanLeftArm.ImageLocation = @"Images\hangman-3.png";
+            hangmanRightArm.ImageLocation = @"Images\hangman-4.png";
+            hangmanLeftLeg.ImageLocation = @"Images\hangman-5.png";
+            hangmanRightLeg.ImageLocation = @"Images\hangman-6.png";
+        }
+    }
+}
