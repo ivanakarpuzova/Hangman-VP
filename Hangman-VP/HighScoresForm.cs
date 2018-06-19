5c43f29c7f4c5254f47ec36fbacf3af7b1fdb4fa
Ivana Karpuzova <karpuzovaivana@gmail.com>
2018-06-19 17:10:54 +0200

    Логиката за да се прикажуваат играчите и нивните рекорди.
    @simonqlumov

--- Hangman-VP/HighScoresForm.cs
+++ Hangman-VP/HighScoresForm.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Hangman.Models;
+using Hangman.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace Hangman
@@ -14,6 +18,51 @@ namespace Hangman
             EasyLabel.Text = LanguageSettings.DifficultyEasy;
             MediumLabel.Text = LanguageSettings.DifficultyMedium;
             HardLabel.Text = LanguageSettings.DifficultyHard;
+
+            List<Player> playersEasy = new List<Player>();
+            List<Player> playersMedium = new List<Player>();
+            List<Player> playersHard = new List<Player>();
+
+            if (Settings.Default.EasyHighscores != null)
+            {
+                foreach (var player in Settings.Default.EasyHighscores)
+                {
+                    string name = player.Trim().Split('-')[0];
+                    int points = Int32.Parse(player.Trim().Split('-')[1]);
+
+                    Player p = new Player(name, points);
+                    playersEasy.Add(p);
+                }
+            }
+
+            if (Settings.Default.MediumHighscores != null)
+            {
+                foreach (var player in Settings.Default.MediumHighscores)
+                {
+                    string name = player.Trim().Split('-')[0];
+                    int points = Int32.Parse(player.Trim().Split('-')[1]);
+
+                    Player p = new Player(name, points);
+                    playersMedium.Add(p);
+                }
+
+            }
+
+            if (Settings.Default.HardHighscores != null)
+            {
+                foreach (var player in Settings.Default.HardHighscores)
+                {
+                    string name = player.Trim().Split('-')[0];
+                    int points = Int32.Parse(player.Trim().Split('-')[1]);
+
+                    Player p = new Player(name, points);
+                    playersHard.Add(p);
+                }
+            }
+
+            PlayersHighScoreListEasy.DataSource = playersEasy.OrderByDescending(x => x.HighScore).ToList();
+            PlayersHighScoreListMedium.DataSource = playersMedium.OrderByDescending(x => x.HighScore).ToList();
+            PlayersHighScoreListHard.DataSource = playersHard.OrderByDescending(x => x.HighScore).ToList();
         }
 
         private void button1_Click(object sender, EventArgs e)
