8880277e3182733aa74dc21e2246f0d0b969e3b1
Ivana Karpuzova <karpuzovaivana@gmail.com>
2018-06-19 14:45:29 +0200

    Дополнителна валидација за CheckBox и RadioButtons.

--- Hangman-VP/NewGameForm.cs
+++ Hangman-VP/NewGameForm.cs
@@ -29,11 +29,69 @@ namespace Hangman_VP
         private void NewGameStartButton_Click(object sender, EventArgs e)
         {
             string name = PlayerNameTextBox.Text;
+            bool hasCheckedCategory = false;
 
             if (String.IsNullOrEmpty(name))
             {
                 MessageBox.Show("Внесете име!", "Невалидно име!");
                 return;
+            }
+
+            Difficulty difficulty;
+            Language language;
+            List<Category> categories = new List<Category>();
+
+            if (DifficultyEasyRadioButton.Checked)
+            {
+                difficulty = Difficulty.Easy;
+            }
+            else if (DifficultyMediumRadioButton.Checked)
+            {
+                difficulty = Difficulty.Medium;
+            }
+            else
+            {
+                difficulty = Difficulty.Hard;
+            }
+
+            if (LanguageEnglishRadioButton.Checked)
+            {
+                language = Language.English;
+            }
+            else
+            {
+                language = Language.Macedonian;
+            }
+
+            foreach (Control checkbox in CategoriesGroupBox.Controls)
+            {
+                if (checkbox is CheckBox)
+                {
+                    if (((CheckBox)checkbox).Checked)
+                    {
+                        hasCheckedCategory = true;
+                        if (checkbox.Text == "General")
+                            categories.Add(Category.General);
+                        if (checkbox.Text == "Sport")
+                            categories.Add(Category.Sport);
+                        if (checkbox.Text == "Science")
+                            categories.Add(Category.Science);
+                        if (checkbox.Text == "Movies")
+                            categories.Add(Category.Movies);
+                        if (checkbox.Text == "Geography")
+                            categories.Add(Category.Geography);
+                    }
+                }
+            }
+
+            if (!hasCheckedCategory)
+            {
+                categories.Add(Category.General);
+                categories.Add(Category.Sport);
+                categories.Add(Category.Science);
+                categories.Add(Category.Movies);
+                categories.Add(Category.Geography);
+
             }
 
             HangmanForm hangmanForm = new HangmanForm();
